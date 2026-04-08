a82a9c8f8f8bf3134a867fbfa911bf6fdd49990f
irfan <md.irfan40@gmail.com>
2026-04-08 23:09:30 +0530

    feat: add new food icons and v2 menu page with routing

--- server.js
+++ server.js
@@ -4,6 +4,14 @@ const path = require('path');
 const app = express();
 const PORT = process.env.PORT || 3000;
 
+app.get('/v2', (req, res) => {
+  res.sendFile(path.join(__dirname, 'v2.html'));
+});
+
+app.get('/v2/', (req, res) => {
+  res.redirect(301, '/v2');
+});
+
 // Serve static files from the current directory
 app.use(express.static(path.join(__dirname)));
 
